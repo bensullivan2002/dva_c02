d1a9b31c4ba764650ddb7566df2127c692c8cd56
Ben Sullivan <bensullivan2002gmail.com>
2025-02-06 21:51:01 +0000

    r

--- src/iac/s3_bucket.py
+++ src/iac/s3_bucket.py
@@ -2,5 +2,5 @@ import boto3
 
 s3 = boto3.client("s3")
 
-# s3.create_bucket(Bucket="my-test-buckt-8374598374", CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
+s3.create_bucket(Bucket="my-test-buckt-8374598374", CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
 s3.upload_file("src/test.json", "my-test-buckt-8374598374", "test.json")
